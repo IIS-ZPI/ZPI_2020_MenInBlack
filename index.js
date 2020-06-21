950c5321e6dd492156cf16a03e858d67a89dc972
Chester193 <46018874+Chester193@users.noreply.github.com>
2020-06-22 01:19:53 +0200

    rename path

--- index.js
+++ index.js
@@ -480,7 +480,7 @@ app.get('/states', (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/taxes-app/build/index.html'));
 });
 
 const port = process.env.PORT || 4000;
